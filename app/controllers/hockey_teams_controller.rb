984589280096e6f2a5c0f75755aad292049753f5
Trevor Suter <Trevorsuter@icloud.com>
2021-02-05 10:01:17 -0700

    user storys 2 and 3

--- app/controllers/hockey_teams_controller.rb
+++ app/controllers/hockey_teams_controller.rb
@@ -7,4 +7,19 @@ class HockeyTeamsController < ApplicationController
   def show
     @hockey_team = HockeyTeam.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    hockey_team = HockeyTeam.new({
+      name: params[:hockey_team][:name],
+      city: params[:hockey_team][:city],
+      rank: params[:hockey_team][:rank],
+      original_franchise: params[:hockey_team][:original_franchise],
+      stanley_cups: params[:hockey_team][:stanley_cups]
+    })
+    hockey_team.save
+    redirect_to '/hockeyteams'
+  end
 end
